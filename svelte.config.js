0980ca5b90c5d422f1ebfc44f0c3dcc5c6f571f1
AxelDevara <axeldevara@gmail.com>
2022-07-05 21:50:15 -0700

    fixed deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-node';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,7 @@ const config = {
   ],
 
   kit: {
-    adapter: netlify(),
+    adapter: adapter(),
     vite: {
       resolve: {
         alias: {
