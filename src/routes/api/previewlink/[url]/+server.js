baa9c4d05c99733f8efdf9afdd4d6b79df8f39b8
AxelDevara <axeldevara@gmail.com>
2023-01-12 11:23:59 +0700

    fixed previewlink

--- src/routes/api/previewlink/[url]/+server.js
+++ src/routes/api/previewlink/[url]/+server.js
@@ -1,10 +1,12 @@
 // throw new Error("@migration task: Update +server.js (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292701)");
 
-import { getLinkPreview, getPreviewFromContent } from 'link-preview-js';
+import { getLinkPreview } from 'link-preview-js';
 
 export const GET = async ({ params }) => {
   const url = params.url;
-  const res = await getLinkPreview(url);
+  const res = await getLinkPreview(url, {
+    timeout: 2000, headers: { "user-agent": "googlebot" }
+  });
 
   return new Response(JSON.stringify({ res }));
 };
