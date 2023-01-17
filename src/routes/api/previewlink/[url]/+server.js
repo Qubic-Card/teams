99f988eee1ea928fa604c5668c03c6bac3daa4fa
AxelDevara <axeldevara@gmail.com>
2023-01-17 13:40:14 +0700

    fixed previewlink errors

--- src/routes/api/previewlink/[url]/+server.js
+++ src/routes/api/previewlink/[url]/+server.js
@@ -1,27 +1,25 @@
 // throw new Error("@migration task: Update +server.js (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292701)");
 
 import { getLinkPreview } from 'link-preview-js';
+const dns = require("node:dns");
 
 export const GET = async ({ params }) => {
   const url = params.url;
   const res = await getLinkPreview(url, {
-    followRedirects: 'manual',
-    handleRedirects: (baseURL, forwardedURL) => {
-      const urlObj = new URL(baseURL);
-      const forwardedURLObj = new URL(forwardedURL);
-      if (
-        forwardedURLObj.hostname === urlObj.hostname ||
-        forwardedURLObj.hostname === 'www.' + urlObj.hostname ||
-        'www.' + forwardedURLObj.hostname === urlObj.hostname
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    timeout: 2000,
-    headers: { 'user-agent': 'googlebot' },
-  });
+    resolveDNSHost: async (url) => {
+      return new Promise((resolve, reject) => {
+        const hostname = new URL(url).hostname;
+        dns.lookup(hostname, (err, address, family) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve(address); // if address resolves to localhost or '127.0.0.1' library will throw an error
+        });
+      });
+    }, timeout: 3000, headers: { "user-agent": "googlebot" }
+  }).catch((e)=> {});
 
   return new Response(JSON.stringify({ res }));
 };
