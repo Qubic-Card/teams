a7c02482d9459db9b649c3679497c308f33a12aa
AxelDevara <axeldevara@gmail.com>
2023-07-31 00:11:42 +0700

    added cancel button

--- src/pages/settings/changepassword.svelte
+++ src/pages/settings/changepassword.svelte
@@ -56,13 +56,20 @@
                 placeholder="Confirm your new password"
                 class="w-full px-4 py-2 mt-2 text-base rounded-md text-black transition duration-500 ease-in-out transform border-transparent bg-neutral-100 focus:border-gray-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
             />
-
-            <button
+            
+                <button
                 disabled={done}
                 on:click={async () => await handleChangePass()}
-                class="block border border-blue-800 disabled:bg-blue-600/60 w-full px-4 py-3 mt-6 font-regular rounded-md text-white transition duration-500 ease-in-out transform bg-blue-600 hover:bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
+                class="border border-blue-800 disabled:bg-blue-600/60 w-full px-4 py-3 mt-6 font-regular rounded-md text-white transition duration-500 ease-in-out transform bg-blue-600 hover:bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
                 >Confirm New Password</button
             >
+            <button
+                disabled={done}
+                on:click={() => isOpen = false}
+                class="border border-red-600 disabled:bg-red-300/60 w-full px-4 py-3 mt-2 font-regular rounded-md text-red-600 transition duration-500 ease-in-out transform  hover:bg-red-200 focus:shadow-outline focus:outline-none bg-red-50 focus:ring-2 ring-offset-current ring-offset-2"
+                >Cancel</button
+            >
+            
         </div>
     </div>
 {/if}
