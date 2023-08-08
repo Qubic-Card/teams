7c353421130c67b5e2894d28b54ea65551aac82f
AxelDevara <axeldevara@gmail.com>
2023-08-08 17:06:48 +0700

    fixed team activation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
   import { toastFailed, toastSuccess } from "@lib/utils/toast";
   import { page } from "$app/stores";
   import { tips } from "@lib/tips";
-    import { goto } from "$app/navigation";
+  import { goto } from "$app/navigation";
 
   const random = Math.floor(Math.random() * tips.length);
   let loading = false;
@@ -40,14 +40,14 @@
   const forgotPassword = () => {
     isForgotPassword = !isForgotPassword;
   };
-  
+
   const handleForgotPassword = async () => {
     try {
       loading = true;
       const { error } = await supabase.auth.resetPasswordForEmail(email);
       if (error) throw error;
       email = "";
-      await goto('/');
+      await goto("/");
       toastSuccess("Check your email!");
       loading = false;
     } catch (error) {
@@ -69,94 +69,97 @@
       <div
         class="justify-start items-center w-full lg:w-[75%] h-screen p-4 lg:p-8"
       >
-        {#if $user}
-          <SelectEditor />
-        {:else if $page.url.searchParams.get("team_id")}
+        {#if $page.url.searchParams.get("team_id")}
           <RegisterTeam />
         {:else}
-          <div
-            class="flex flex-col justify-between h-full w-full py-8 rounded-2xl px-8 bg-white text-black"
-          >
-            <div class="text-xl">
-              <div class="flex items-center">
-                <img src="/qubic-black.svg" alt="" width="32" height="32" />
-                <span class="text-xl md:text-2xl font-semibold ml-2"
-                  >Editor</span
-                >
-              </div>
-            </div>
-
-            <div class="mx-auto w-[75%]">
-              {#if isForgotPassword}
-                <h1
-                  class="mt-6 text-3xl mb-4 font-semibold tracking-tighter text-left sm:text-3xl title-font"
-                >
-                  Make a new password
-                </h1>
-              {:else}
-                <div class="pb-8">
-                  <h1 class="text-3xl font-semibold">Welcome back</h1>
-                  <p class="text-neutral-500 text-sm">
-                    Log in to make changes to your profile
-                  </p>
+          {#if $user}
+            <SelectEditor />
+          {:else}
+            <div
+              class="flex flex-col justify-between h-full w-full py-8 rounded-2xl px-8 bg-white text-black"
+            >
+              <div class="text-xl">
+                <div class="flex items-center">
+                  <img src="/qubic-black.svg" alt="" width="32" height="32" />
+                  <span class="text-xl md:text-2xl font-semibold ml-2"
+                    >Editor</span
+                  >
                 </div>
-              {/if}
-              <p class="block text-sm text-left">Email Address</p>
-              <input
-                bind:value={email}
-                type="email"
-                placeholder="Your Email"
-                class="w-full px-4 py-2 mt-2 text-base rounded-md text-black transition duration-500 ease-in-out transform border-transparent bg-neutral-100 focus:border-gray-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
-              />
-              {#if !isForgotPassword}
-                <p class="block text-sm mt-2 text-left">Password</p>
+              </div>
+
+              <div class="mx-auto w-[75%]">
+                {#if isForgotPassword}
+                  <h1
+                    class="mt-6 text-3xl mb-4 font-semibold tracking-tighter text-left sm:text-3xl title-font"
+                  >
+                    Make a new password
+                  </h1>
+                {:else}
+                  <div class="pb-8">
+                    <h1 class="text-3xl font-semibold">Welcome back</h1>
+                    <p class="text-neutral-500 text-sm">
+                      Log in to make changes to your profile
+                    </p>
+                  </div>
+                {/if}
+                <p class="block text-sm text-left">Email Address</p>
                 <input
-                  bind:value={password}
-                  on:keypress={onKeyPress}
-                  type="password"
-                  placeholder="Your Password"
+                  bind:value={email}
+                  type="email"
+                  placeholder="Your Email"
                   class="w-full px-4 py-2 mt-2 text-base rounded-md text-black transition duration-500 ease-in-out transform border-transparent bg-neutral-100 focus:border-gray-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
                 />
-              {/if}
-              {#if !loading}
-                {#if isForgotPassword}
-                  <button
-                    disabled={loading}
-                    on:click={async () => await handleForgotPassword()}
-                    class="block w-full px-4 py-3 md:mt-6 font-regular rounded-md text-white transition duration-500 ease-in-out transform bg-blue-500 hover:bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
-                    >Send Email</button
-                  >
-                {:else}
-                  <button
-                    disabled={loading}
-                    on:click={async () => await handleLogin()}
-                    class="block border border-blue-800 disabled:bg-blue-600/60 w-full px-4 py-3 mt-6 font-regular rounded-md text-white transition duration-500 ease-in-out transform bg-blue-600 hover:bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
-                    >Log In</button
-                  >
+                {#if !isForgotPassword}
+                  <p class="block text-sm mt-2 text-left">Password</p>
+                  <input
+                    bind:value={password}
+                    on:keypress={onKeyPress}
+                    type="password"
+                    placeholder="Your Password"
+                    class="w-full px-4 py-2 mt-2 text-base rounded-md text-black transition duration-500 ease-in-out transform border-transparent bg-neutral-100 focus:border-gray-500 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                  />
                 {/if}
-              {:else}
-                <Spinner class="h-8 w-8 my-4" />
-              {/if}
-              <p
-                class="mt-2 text-neutral-600 text-sm cursor-pointer w-56"
-                on:click={forgotPassword}
-              >
-                {isForgotPassword ? "Back" : "Forgot password?"}
-              </p>
-            </div>
+                {#if !loading}
+                  {#if isForgotPassword}
+                    <button
+                      disabled={loading}
+                      on:click={async () => await handleForgotPassword()}
+                      class="block w-full px-4 py-3 md:mt-6 font-regular rounded-md text-white transition duration-500 ease-in-out transform bg-blue-500 hover:bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
+                      >Send Email</button
+                    >
+                  {:else}
+                    <button
+                      disabled={loading}
+                      on:click={async () => await handleLogin()}
+                      class="block border border-blue-800 disabled:bg-blue-600/60 w-full px-4 py-3 mt-6 font-regular rounded-md text-white transition duration-500 ease-in-out transform bg-blue-600 hover:bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
+                      >Log In</button
+                    >
+                  {/if}
+                {:else}
+                  <Spinner class="h-8 w-8 my-4" />
+                {/if}
+                <p
+                  class="mt-2 text-neutral-600 text-sm cursor-pointer w-56"
+                  on:click={forgotPassword}
+                >
+                  {isForgotPassword ? "Back" : "Forgot password?"}
+                </p>
+              </div>
 
-            <div>
-              <p
-                class="text-neutral-600 text-sm cursor-pointer w-56"
-                on:click={() =>
-                  window.open("https://qubic.id/teams", "_blank").focus()}
-              >
-                Learn about qubic teams ↗
-              </p>
+              <div>
+                <p
+                  class="text-neutral-600 text-sm cursor-pointer w-56"
+                  on:click={() =>
+                    window.open("https://qubic.id/teams", "_blank").focus()}
+                >
+                  Learn about qubic teams ↗
+                </p>
+              </div>
             </div>
-          </div>
+          {/if}
+
+          <div class="bg-black lg:w-full" />
         {/if}
-        <div class="bg-black lg:w-full" />
       </div>
       <div
         class=" h-full w-full items-left justify-center hidden lg:flex flex-col"
